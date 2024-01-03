d2163bb6b984303fdfac99150a8d769e398c1c4a
Lorenzo Salvadore <hacker@lorenzosalvadore.it>
2024-01-03 11:32:12 +0100

    revshell: Add command
    Add a command to generate reverse shells.

---
+++ backend/revshell.py
@@ -0,0 +1,19 @@
+from base64 import b64encode
+from re import sub
+
+def revshell(args):
+    language = args.language
+    encoding = args.encoding
+    attackerIP = args.attacker_ip
+    attackerPort = args.attacker_port
+    with open(f"data/reverse shells/{language}","r") as code:
+        shell = b""
+        for line in code:
+            line = sub("ATTACKER_IP",attackerIP,line)
+            line = sub("ATTACKER_PORT",attackerPort,line)
+            shell += line.encode()
+    if encoding == "base64":
+        output = b64encode(shell).decode()
+    elif encoding == "none":
+        output = shell.decode()
+    print(output)
